9873f3d58b78ef757e3bc67d632b198fd38d4bb8
ybbbb567 <985422523@qq.com>
2023-03-28 03:54:31 +0000

    Code Synced by DhiWise

--- src/pages/Notification/index.jsx
+++ src/pages/Notification/index.jsx
@@ -8,7 +8,7 @@ const NotificationPage = () => {
       <div className="bg-bluegray_100_a2 flex font-montserrat items-center justify-start mx-[auto] w-[100%]">
         <div
           className="bg-cover bg-no-repeat flex h-[1491px] items-end justify-start p-[270px] sm:px-[20px] md:px-[40px] w-[100%]"
-          style={{ backgroundImage: "url('images/img_group63.png')" }}
+          style={{ backgroundImage: "url('images/img_group154.png')" }}
         >
           <Text
             className="font-bold mb-[460px] mt-[430px] text-deep_orange_A700 text-left tracking-[-0.72px] w-[auto]"
--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,64 +1,55 @@
 import React from "react";
 
-import { Text, Img } from "components";
 import Navigationbar from "components/Navigationbar";
+import { Text, Img } from "components";
 import Footer from "components/Footer";
 
 const SearchPagePage = () => {
   return (
     <>
-      <div className="bg-gradient  flex flex-col font-opensans sm:gap-[40px] md:gap-[40px] gap-[441px] items-center justify-start mx-[auto] w-[100%]">
-        <div className="md:h-[1071px] sm:h-[1107px] h-[1320px] md:px-[20px] relative w-[100%]">
-          <div className="absolute bg-white_A700 bottom-[0] flex flex-col md:gap-[40px] gap-[76px] inset-x-[0] justify-start mx-[auto] pr-[122px] sm:pr-[20px] md:pr-[40px] py-[122px] w-[99%]">
-            <Text
-              className="font-semibold md:ml-[0] ml-[292px] text-black_900 text-left tracking-[-0.72px] w-[auto]"
-              as="h1"
-              variant="h1"
-            >
-              Type the link of the website you want access below
-            </Text>
-            <div className="font-dmsans h-[500px] md:h-[721px] mb-[221px] mr-[294px] relative md:w-[100%] w-[81%]">
+      <div className="bg-gradient  flex flex-col font-opensans items-center justify-start mx-[auto] w-[100%]">
+        <Navigationbar
+          className="flex items-center justify-center md:px-[20px] w-[100%]"
+          home="Home"
+          language="Chinese"
+          picwishone="images/img_picwish2_125x227.png"
+        />
+        <div className="flex flex-col md:gap-[40px] gap-[76px] justify-start mt-[21px] pr-[122px] sm:pr-[20px] md:pr-[40px] py-[122px] w-[100%]">
+          <Text
+            className="font-semibold md:ml-[0] ml-[312px] text-black_900 text-left tracking-[-0.72px] w-[auto]"
+            as="h1"
+            variant="h1"
+          >
+            Type the link of the website you want access below
+          </Text>
+          <div className="font-dmsans h-[500px] md:h-[721px] mb-[221px] mr-[314px] relative md:w-[100%] w-[80%]">
+            <Img
+              src="images/img_searchbar.svg"
+              className="absolute h-[36px] right-[8%] top-[25%] w-[930px]"
+              alt="searchbar"
+            />
+            <Img
+              src="images/img_websitebackgro_500x500.png"
+              className="absolute h-[500px] inset-y-[0] left-[0] my-[auto] object-cover w-[500px]"
+              alt="websitebackgro"
+            />
+            <div className="absolute h-[102px] right-[0] top-[31%] md:w-[100%] w-[58%]">
+              <Text
+                className="ml-[124px] my-[auto] not-italic text-left text-white_A700 w-[auto]"
+                as="h6"
+                variant="h6"
+              >
+                Check
+              </Text>
               <Img
-                src="images/img_searchbar.svg"
-                className="absolute h-[36px] right-[9%] top-[25%] w-[930px]"
-                alt="searchbar"
+                src="images/img_component4.png"
+                className="absolute h-[102px] inset-[0] justify-center m-[auto] object-cover w-[auto]"
+                alt="componentFour"
               />
-              <Img
-                src="images/img_websitebackgro.png"
-                className="absolute h-[500px] inset-y-[0] left-[0] my-[auto] object-cover w-[39%]"
-                alt="websitebackgro"
-              />
-              <div className="absolute bg-gradient1  bottom-[36%] flex md:flex-col flex-row gap-[48px] items-center justify-center p-[23px] sm:px-[20px] right-[0] rounded-[15px] w-[58%]">
-                <div className="h-[47px] md:ml-[0] ml-[5px] md:mt-[0] mt-[7px] relative w-[47px]">
-                  <Img
-                    src="images/img_car.svg"
-                    className="h-[16px] ml-[5px] mt-[5px] w-[auto]"
-                    alt="car"
-                  />
-                  <Img
-                    src="images/img_search.svg"
-                    className="absolute h-[47px] inset-[0] justify-center m-[auto] w-[47px]"
-                    alt="search"
-                  />
-                </div>
-                <Text
-                  className="mr-[492px] not-italic text-left text-white_A700 w-[auto]"
-                  as="h5"
-                  variant="h5"
-                >
-                  Check
-                </Text>
-              </div>
             </div>
           </div>
-          <Navigationbar
-            className="absolute h-[278px] inset-x-[0] mx-[auto] top-[0] w-[100%]"
-            home="Home"
-            picwishone="images/img_picwish1.png"
-            language="Chinese"
-          />
         </div>
-        <Footer className="flex font-spacegrotesk items-center justify-center md:px-[20px] w-[100%]" />
+        <Footer className="flex font-spacegrotesk items-center justify-center mt-[374px] md:px-[20px] w-[100%]" />
       </div>
     </>
   );
