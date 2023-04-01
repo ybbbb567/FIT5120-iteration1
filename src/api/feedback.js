7b7e1cfe493aee53f97cc62ccea90b96e0b48852
yxuu0152 <yxuu0152@student.monash.edu>
2023-04-01 18:26:51 +1100

    add feedback api modified

--- src/api/feedback.js
+++ src/api/feedback.js
@@ -3,7 +3,14 @@ import request from '../utils/request'
 export function addFeedback (feedback) {
   return request({
     url: `/feedback/a`,
+    data: feedback,
     method: 'post',
-    data: feedback
+  })
+}
+
+export function getFeedback (){
+  return request({
+    url: `/feedback/all`,
+    method: 'get',
   })
 }
