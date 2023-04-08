9e0b1c731656be54971d5ca3b4bdcd321b50c5ef
ybbbb567 <985422523@qq.com>
2023-04-08 22:53:26 +1000

    change the gap of navigationbar

--- src/components/Navigationbar/index.jsx
+++ src/components/Navigationbar/index.jsx
@@ -12,32 +12,35 @@ const Navigationbar = (props) => {
 
   return (
     <>
-    
+
       <div className={props.className}>
         <div className="flex items-center justify-start mx-[auto] w-[100%]">
-          <div className="navbar_color flex flex-row gap-[400px] items-center justify-start pr-[1318px] sm:pr-[20px] md:pr-[40px] w-[100%]">
+          <div className="navbar_color flex flex-row gap-[300px] items-center justify-start pr-[1318px] sm:pr-[20px] md:pr-[40px] w-[100%]">
             <Img
               src={props?.picwishone}
               className="h-[145px] md:h-[auto] object-cover "
               alt="picwishOne"
             />
             <div className="flex items-center justify-start w-[auto]">
-            <NavLink to="/" style={({ isActive }) => ({ 
-                            color: isActive ? '#f15f79' : 'white' , borderBottom: isActive ? '3px solid #f15f79':'',fontSize: 20, fontWeight: 'bold'})} >Home</NavLink>
-      
-            </div>
-            <div className="flex items-center justify-start w-[auto]">
-            <NavLink to="/searchpage" style={({ isActive }) => ({ 
-                            color: isActive ? '#f15f79' : 'white' , borderBottom: isActive ? '3px solid #f15f79':'',fontSize: 20, fontWeight: 'bold'})} ><nobr>URL Checker</nobr></NavLink>
-
-
-
+              <NavLink to="/" style={({ isActive }) => ({
+                color: isActive ? '#f15f79' : 'white', borderBottom: isActive ? '3px solid #f15f79' : '', fontSize: 20, fontWeight: 'bold'
+              })} >Home</NavLink>
 
             </div>
             <div className="flex items-center justify-start w-[auto]">
-            <NavLink to="/quiz" style={({ isActive }) => ({ 
-                            color: isActive ? '#f15f79' : 'white' , borderBottom: isActive ? '3px solid #f15f79':'',fontSize: 20, fontWeight: 'bold'})} >Quiz</NavLink>
-  </div>
+              <NavLink to="/searchpage" style={({ isActive }) => ({
+                color: isActive ? '#f15f79' : 'white', borderBottom: isActive ? '3px solid #f15f79' : '', fontSize: 20, fontWeight: 'bold'
+              })} ><nobr>URL Checker</nobr></NavLink>
+
+
+
+
+            </div>
+            <div className="flex items-center justify-start w-[auto]">
+              <NavLink to="/quiz" style={({ isActive }) => ({
+                color: isActive ? '#f15f79' : 'white', borderBottom: isActive ? '3px solid #f15f79' : '', fontSize: 20, fontWeight: 'bold'
+              })} >Quiz</NavLink>
+            </div>
 
           </div>
         </div>
