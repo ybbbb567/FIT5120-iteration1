d1a6305341efe2f0b6956d754c5baad15f91ad21
Lambert Li <ylii0159@student.monash.edu>
2023-04-09 22:28:15 +1000

    Add share function to quiz

--- src/pages/QuizPage/index.jsx
+++ src/pages/QuizPage/index.jsx
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 import { Card, Radio, Result, Space } from 'antd';
 import { getAllQuiz } from "api/quiz";
 import { useEffect } from "react";
+import { FacebookShareButton, FacebookIcon ,TwitterShareButton, TwitterIcon} from 'react-share';
 
 
   const handleReload = () => {
@@ -111,6 +112,21 @@ const QuizPage = () => {
                 />
               </div>
             )}
+            <div className="  mx-auto cursor-pointer font-bold font-montserrat leading-[normal]  py-5  text-center text-black text-xl tracking-[-0.30px] w-1/6">
+  <TwitterShareButton
+    url={'https://www.trustify.one/quiz'}
+    hashtag="#web security"
+    style={{ marginRight: '10px' }}
+  >
+    <TwitterIcon size={32} round />
+  </TwitterShareButton>
+  <FacebookShareButton
+    url={'https://www.trustify.one/quiz'}
+    hashtag="#web security"
+  >
+    <FacebookIcon size={32} round />
+  </FacebookShareButton>
+</div>
               <Button className=" bg-white rounded-full mx-auto border border-blue_500 border-solid cursor-pointer font-bold font-montserrat leading-[normal]  py-5  text-center text-black text-xl tracking-[-0.30px] w-1/6"
                onClick={handleReload}>
                   Try again
