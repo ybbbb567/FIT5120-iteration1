a79b64353607116cf7a0c9f05cc42dd364511883
Lambert Li <ylii0159@student.monash.edu>
2023-05-16 00:54:57 +1000

    Change text error in information page

--- src/pages/InformationPage/index.jsx
+++ src/pages/InformationPage/index.jsx
@@ -150,7 +150,7 @@ const InformationPage = () => {
     <li aria-current="page">
       <div class="flex items-center">
         <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-        <span class="text-xl	 ml-1 text-sm font-medium text-purple md:ml-2 dark:text-purple">Simulation</span>
+        <span class="text-xl	 ml-1 text-sm font-medium text-purple md:ml-2 dark:text-purple">Information</span>
       </div>
     </li>
   </ol>
