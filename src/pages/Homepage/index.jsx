15b3ea3f437903d65e1ea282b85516f0ec6e62fc
Lambert Li <ylii0159@student.monash.edu>
2023-05-15 20:17:52 +1000

    Add information and simulation section to homepage

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -174,7 +174,7 @@ const HomepagePage = () => {
                   <h3 className="text-3xl font-semibold text-blueGray"> Quickly identify the risks of the SMS you receive
                   </h3>
                   <p className="mt-4 text-lg leading-relaxed ">
-                    SMS Detector identifies scam texts by analyzing their content.
+                    Email Detector identifies scam texts by analyzing their content.
                     It detects suspicious links, requests for personal info, and common scam phrases.
                     This helps users stay safe from fraud.
                   </p>
@@ -252,6 +252,98 @@ const HomepagePage = () => {
             </div>
           </div>
         </section>
+
+<section className="relative py-20">
+
+
+<div className="container mx-auto px-4">
+  <div className="items-center flex ">
+    <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
+      <div className="md:pr-12">
+
+        <h3 className="text-3xl font-semibold text-blueGray"> What you need to know about online scam</h3>
+
+        <p className="mt-4 text-lg leading-relaxed ">
+        The  section is where fraud relative information is provided for users. It not only has the correct reporting channels for fraud, but also has links to relevant courses and lectures provided by the government and anti-fraud-related organizations, so that people can 
+        learn about different types of fraud and how to avoid them by accessing those contents.
+        </p>
+        <ul className="list-none mt-6">
+          <li className="py-2">
+            <div className="flex items-center">
+              <Button className="btn cta bg"
+
+                onClick={() => navigate("/information")}
+              >
+                Check your SMS
+              </Button>
+            </div>
+
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
+      <Img
+        src="images/Information.png"
+        className="md:flex-1 h-[663px] sm:h-auto object-cover w-auto md:w-full"
+        alt="information"
+      />
+    </div>
+
+  </div>
+</div>
+</section>
+
+
+
+
+<section className="relative py-20">
+
+
+          <div className="container mx-auto px-4">
+            <div className="items-center flex ">
+              <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
+                <Img
+                  src="images/Simulation.png"
+                  className="md:flex-1 h-[663px] sm:h-auto object-cover w-auto md:w-full"
+                  alt="simulation"
+                />
+              </div>
+              <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
+                <div className="md:pr-12">
+
+                  <h3 className="text-3xl font-semibold text-blueGray"> Simulate a real-life scam scenario
+
+
+
+
+
+                  </h3>
+                  <p className="mt-4 text-lg leading-relaxed ">
+                  Our simulation is based on ChatGPT as an intelligent simulated dialogue designed by an anonymous fraudster. 
+                  The aim is to help users identify some real or fictitious fraud instances, so as to increase their vigilance when encountering fraudulent conversations in the real world and avoid fraud crises.
+                  </p>
+                  <ul className="list-none mt-6">
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <Button className="btn cta bg"
+
+                          onClick={() => navigate("/simulate")}
+                        >
+                          View the Map
+                        </Button>
+                      </div>
+
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+
 
 
 
