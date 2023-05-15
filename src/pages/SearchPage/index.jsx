c038e5d44f800dcf3a40dcbf77d7e66c5768bd78
Lambert Li <ylii0159@student.monash.edu>
2023-05-16 00:59:23 +1000

    Add breadcrumb nav in url checker

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -176,7 +176,26 @@ const SearchPagePage = () => {
       <div className="navbar_color   mx-[auto] w-[100%]">
         <Navigationbar
         />
-        <div className="flex flex-col md:gap-[40px] gap-[76px] justify-start mt-[21px] pr-[122px] sm:pr-[20px] md:pr-[40px] py-[122px] w-[100%]">
+  
+        <div
+          className="z-0 font-pacifico h-max	m-max  mx-auto p-[129px] md:px-5 relative w-full">
+
+<nav class="flex pt-10 py-10" aria-label="Breadcrumb">
+  <ol class="inline-flex items-center space-x-1 md:space-x-3">
+    <li class="inline-flex items-center">
+      <a href="#" class="inline-flex text-xl items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+        <svg aria-hidden="true" class="w- 4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+        Home
+      </a>
+    </li>
+    <li aria-current="page">
+      <div class="flex items-center">
+        <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+        <span class="text-xl	 ml-1 text-sm font-medium text-purple md:ml-2 dark:text-purple">URL Checker</span>
+      </div>
+    </li>
+  </ol>
+</nav>
 
           
           <div className="font-dmsans h-[700px] md:h-[721px] mb-[221px] mr-[314px] relative md:w-[100%] w-[80%]">
