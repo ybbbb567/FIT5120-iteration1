1fb5336ac3c5d1a9e1b1c6c67691a10e062bce9c
ybbbb567 <985422523@qq.com>
2023-05-18 16:51:22 +1000

    add loading

--- src/pages/SMSDetector/index.jsx
+++ src/pages/SMSDetector/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 import Navigationbar from "components/Navigationbar";
 import Footer from "components/Footer";
 import { checkSMS } from "api/check";
@@ -8,6 +8,8 @@ import Highlighter from 'react-highlight-words';
 
 
 const SMSDetectorPage = () => {
+
+  const [loading, setLoading] = useState(false);
 
   const [inputValue, setInputValue] = useState("");
 
@@ -33,9 +35,11 @@ const SMSDetectorPage = () => {
       return;
     }
 
+    setLoading(true)
     checkSMS(inputValue).then(res => {
       if (res.result) {
         setResult(res.result);
+        setLoading(false)
       }
     })
   };
@@ -44,28 +48,28 @@ const SMSDetectorPage = () => {
     <>
       <div className="navbar_color  mx-[auto] w-[100%]">
         <Navigationbar
-          
+
         />
 
         <div className="font-pacifico h-full  mx-auto p-[129px] md:px-5 relative w-full">
 
-          
-        <nav class="flex pt-10 py-10" aria-label="Breadcrumb">
-  <ol class="inline-flex items-center space-x-1 md:space-x-3">
-    <li class="inline-flex items-center">
-      <a href="#" class="inline-flex text-xl items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-        <svg aria-hidden="true" class="w- 4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
-        Home
-      </a>
-    </li>
-    <li aria-current="page">
-      <div class="flex items-center">
-        <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-        <span class="text-xl	 ml-1 text-sm font-medium text-purple md:ml-2 dark:text-purple">Email Detector</span>
-      </div>
-    </li>
-  </ol>
-</nav>
+
+          <nav class="flex pt-10 py-10" aria-label="Breadcrumb">
+            <ol class="inline-flex items-center space-x-1 md:space-x-3">
+              <li class="inline-flex items-center">
+                <a href="#" class="inline-flex text-xl items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                  <svg aria-hidden="true" class="w- 4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+                  Home
+                </a>
+              </li>
+              <li aria-current="page">
+                <div class="flex items-center">
+                  <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                  <span class="text-xl	 ml-1 text-sm font-medium text-purple md:ml-2 dark:text-purple">Email Detector</span>
+                </div>
+              </li>
+            </ol>
+          </nav>
 
 
           <h3 className="text-3xl text-center font-bold text-blueGray" >Email Detector</h3>
@@ -132,6 +136,7 @@ const SMSDetectorPage = () => {
               display: 'block', width: 700
             }}>
               <Card
+                loading={loading}
                 centered="true"
                 title="Classification Result"
                 headStyle={{ backgroundColor: " #C84E89", fontSize: "24px" }}
